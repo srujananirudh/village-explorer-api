20010a80853bad4d09526141770fc9b885813290
Srujan Anirudh Gurjapalli <srujananirudh8@gmail.com>
2026-04-09 21:23:19 +0530

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -26,11 +26,9 @@ def home():
     return {"message": "API running 🚀"}
 
 # ⚠️ TEMP LOCAL DB (will change later)
-DATABASE_URL = "postgresql://postgres:Srujananirudh8@db.ddojoxpahvbchydnwfqc.supabase.co:5432/postgres"
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"sslmode": "require"}
-)
+DATABASE_URL = "postgresql://postgres:Srujananirudh8@db.ddojoxpahvbchydnwfqc.supabase.co:5432/postgres?sslmode=require"
+
+engine = create_engine(DATABASE_URL)
 # ✅ USER MODEL
 class User(BaseModel):
     email: str
